73d36abb3eb441fe9996c8e08eaab32dcea4c50e
Hung Nguyen <nthungdev@gmail.com>
2019-03-04 21:11:37 -0500

    finish flutter chatroom app

--- flutter-demo/lib/main.dart
+++ flutter-demo/lib/main.dart
@@ -1,111 +1,130 @@
+import 'package:demo/message.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(FirebaseChatroom());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FirebaseChatroom extends StatefulWidget {
+  const FirebaseChatroom({Key key}) : super(key: key);
+
+  @override
+  FirebaseChatroomState createState() => FirebaseChatroomState();
+}
+
+class FirebaseChatroomState extends State<FirebaseChatroom> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _messageControler = TextEditingController();
+
+  CollectionReference _chatroomRef;
+
+  void initState() {
+    super.initState();
+
+    _chatroomRef = Firestore.instance.collection("chatroom");
+  }
+
+  Widget _buildMessage(String name, String message) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, top: 10, bottom: 10),
+      child: Container(
+          child: Row(
+        children: <Widget>[
+          Text(
+            "$name: ",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(message, style: TextStyle(fontSize: 16))
+        ],
+      )),
+    );
+  }
+
+  _sendMessage() {
+    print(_nameController.text);
+    print(_messageControler.text);
+
+    var message =
+        Message(_nameController.text, _messageControler.text, DateTime.now());
+    _chatroomRef.add(message.toJson());
+
+    _messageControler.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Firebase Chatroom"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            body: Column(
+              children: <Widget>[
+                StreamBuilder<QuerySnapshot>(
+                  stream: _chatroomRef.snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData || snapshot.data.documents.isEmpty) {
+                      return Expanded(
+                          child: Center(child: Text("No one said anything")));
+                    } else if (snapshot.hasError) {
+                      print(snapshot.error);
+                      return Expanded(
+                          child: Center(child: Text(snapshot.error)));
+                    } else {
+                      List<DocumentSnapshot> sorted = snapshot.data.documents;
+                      sorted.sort((a, b) {
+                        DateTime aTime = a.data["time"];
+                        DateTime bTime = b.data["time"];
+                        return aTime.compareTo(bTime);
+                      });
+
+                      return Expanded(
+                        child: ListView.builder(
+                          itemCount: sorted.length,
+                          itemBuilder: (context, index) {
+                            return _buildMessage(sorted[index].data["name"],
+                                sorted[index].data["message"]);
+                          },
+                        ),
+                      );
+                    }
+                  },
+                ),
+                Divider(height: 1.0),
+                Material(
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration:
+                                      InputDecoration(hintText: "Your name"),
+                                  controller: _nameController,
+                                ),
+                                TextFormField(
+                                  onEditingComplete: _sendMessage,
+                                  decoration:
+                                      InputDecoration(hintText: "Your message"),
+                                  controller: _messageControler,
+                                ),
+                              ],
+                            ),
+                          ),
+                          IconButton(
+                            splashColor: Colors.red,
+                            onPressed: _sendMessage,
+                            icon: Icon(Icons.send, color: Colors.blue),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )));
   }
 }
